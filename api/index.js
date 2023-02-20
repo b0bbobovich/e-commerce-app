2075842bf3cdb432eb49b357499d4c503fe0228c
Andrew <bobrovskyandrew@gmail.com>
2023-02-20 13:41:56 +0200

    update cors policy config

--- api/index.js
+++ api/index.js
@@ -15,11 +15,20 @@ const app = express();
 dotenv.config();
 
 
-const corsOptions = {
-    origin: ["http://localhost:3000", "http://localhost:3001", "https://boboshop.onrender.com", "https://boboshop-admin.onrender.com"],
-}
+const allowedOrigins = ["http://localhost:3000", "http://localhost:3001", "https://boboshop.onrender.com", "https://boboshop-admin.onrender.com"];
 
-app.use(cors(corsOptions));
+
+app.use(cors({
+    origin: function(origin, callback){
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        const msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  }));
 
 mongoose
     .connect(process.env.MONGO_URL)
