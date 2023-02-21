91e227726d673f03183ef506e64e7e73f4786e42
Andrew <bobrovskyandrew@gmail.com>
2023-02-21 14:32:07 +0200

    change base_url to prod_url

--- frontend/src/requestsMethods.js
+++ frontend/src/requestsMethods.js
@@ -7,7 +7,7 @@ const DEV_URL = "http://localhost:5000/api/v1";
 
 
 export const publicRequest = axios.create({
-    baseURL: DEV_URL
+    baseURL: PROD_URL
 });
 
 export const userRequest = () => {
