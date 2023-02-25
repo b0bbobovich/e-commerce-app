f2928c8e1d837bffc102d3b5095f8a6fa77d6da3
Andrew <bobrovskyandrew@gmail.com>
2023-02-25 19:31:40 +0200

    updated cors option list for allowed domain names, removed /api subfolder

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const productRoute = require('./routes/products');
@@ -10,17 +9,20 @@ const cartRoute = require('./routes/cart');
 const ordersRoute = require('./routes/orders');
 
 
-
 const app = express();
 dotenv.config();
 
+const corsOptions = {
+    origin: [
+        "https://bbshop.netlify.app",
+        "http://localhost:3001",
+        "http://localhost:3001",
+        "https://boboshop.onrender.com",
+        "https://boboshop-admin.onrender.com",
+    ],
+}
 
-// const corsOptions = {
-//     origin: ["http://localhost:3001", "http://localhost:3001", "https://boboshop.onrender.com", "https://boboshop-admin.onrender.com"],
-// }
-
-app.use(cors());
-
+app.use(cors(corsOptions));
 
 mongoose
     .connect(process.env.MONGO_URL)
@@ -31,14 +33,12 @@ mongoose
         console.log(err);
     });
 
-
-
 app.use(express.json());
-app.use('/api/v1/auth', authRoute);
-app.use('/api/v1/users', userRoute);
-app.use('/api/v1/products', productRoute);
-app.use('/api/v1/cart', cartRoute);
-app.use('/api/v1/orders', ordersRoute);
+app.use('/v1/auth', authRoute);
+app.use('/v1/users', userRoute);
+app.use('/v1/products', productRoute);
+app.use('/v1/cart', cartRoute);
+app.use('/v1/orders', ordersRoute);
 
 
 app.listen(process.env.PORT || 5000, () => {
