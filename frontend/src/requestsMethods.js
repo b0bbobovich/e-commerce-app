25cad3d0f362fdd6fd6bf9bd96ef53a603c27b4b
Andrew <bobrovskyandrew@gmail.com>
2023-12-31 17:13:02 +0200

    change dev creds to prod

--- frontend/src/requestsMethods.js
+++ frontend/src/requestsMethods.js
@@ -5,12 +5,12 @@ const PROD_URL = 'https://bbshop-api.cyclic.app/v1';
 const DEV_URL = 'http://localhost:5000/v1';
 
 export const publicRequest = axios.create({
-  baseURL: DEV_URL,
+  baseURL: PROD_URL,
 });
 
 export const userRequest = () => {
   return axios.create({
-    baseURL: DEV_URL,
+    baseURL: PROD_URL,
     headers: { token: `Bearer ${getToken()}` },
   });
 
